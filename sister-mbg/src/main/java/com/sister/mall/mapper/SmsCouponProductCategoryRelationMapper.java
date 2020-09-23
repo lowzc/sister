328625af0d5bc6af663df55cd6c4e496dc3c3096
Hzcheng1012 <huangzucheng>
2020-09-23 16:18:51 +0800

    chushihua

---
+++ sister-mbg/src/main/java/com/sister/mall/mapper/SmsCouponProductCategoryRelationMapper.java
@@ -0,0 +1,30 @@
+package com.sister.mall.mapper;
+
+import com.sister.mall.model.SmsCouponProductCategoryRelation;
+import com.sister.mall.model.SmsCouponProductCategoryRelationExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface SmsCouponProductCategoryRelationMapper {
+    long countByExample(SmsCouponProductCategoryRelationExample example);
+
+    int deleteByExample(SmsCouponProductCategoryRelationExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(SmsCouponProductCategoryRelation record);
+
+    int insertSelective(SmsCouponProductCategoryRelation record);
+
+    List<SmsCouponProductCategoryRelation> selectByExample(SmsCouponProductCategoryRelationExample example);
+
+    SmsCouponProductCategoryRelation selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") SmsCouponProductCategoryRelation record, @Param("example") SmsCouponProductCategoryRelationExample example);
+
+    int updateByExample(@Param("record") SmsCouponProductCategoryRelation record, @Param("example") SmsCouponProductCategoryRelationExample example);
+
+    int updateByPrimaryKeySelective(SmsCouponProductCategoryRelation record);
+
+    int updateByPrimaryKey(SmsCouponProductCategoryRelation record);
+}
---
+++ sister-mbg/src/main/java/com/sister/mall/mapper/SmsFlashPromotionSessionMapper.java
@@ -0,0 +1,30 @@
+package com.sister.mall.mapper;
+
+import com.sister.mall.model.SmsFlashPromotionSession;
+import com.sister.mall.model.SmsFlashPromotionSessionExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface SmsFlashPromotionSessionMapper {
+    long countByExample(SmsFlashPromotionSessionExample example);
+
+    int deleteByExample(SmsFlashPromotionSessionExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(SmsFlashPromotionSession record);
+
+    int insertSelective(SmsFlashPromotionSession record);
+
+    List<SmsFlashPromotionSession> selectByExample(SmsFlashPromotionSessionExample example);
+
+    SmsFlashPromotionSession selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") SmsFlashPromotionSession record, @Param("example") SmsFlashPromotionSessionExample example);
+
+    int updateByExample(@Param("record") SmsFlashPromotionSession record, @Param("example") SmsFlashPromotionSessionExample example);
+
+    int updateByPrimaryKeySelective(SmsFlashPromotionSession record);
+
+    int updateByPrimaryKey(SmsFlashPromotionSession record);
+}
